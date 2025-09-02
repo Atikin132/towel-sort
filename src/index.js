6b0bb7e7fff49186abc96af6804f5e1c3359a97a
Atikin132 <nikita.kupriyan0v@yahoo.com>
2025-09-02 17:42:51 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (matrix) {
+    return matrix
+      .map((x, index) => {
+        if ((index + 1) % 2 === 0) {
+          return x.reverse();
+        }
+        return x;
+      })
+      .flat();
+  }
+  return [];
 };
